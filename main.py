740d045a5740d27841a249aeca0e0cef114e0c66
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-16 17:16:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ with container_titulo:
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 # Lê os dados de um arquivo Excel online
-dados = conn.read(worksheet='Tabela1', usecols=list(range(17)))
+dados = conn.read(worksheet='Tabela1', usecols=list(range(16)))
 
 #ws = conn.get_worksheet('Tabela1')
 
