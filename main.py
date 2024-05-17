12858de6846df29eecd6abbeee47f3dddd574261
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-16 22:21:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -140,15 +140,8 @@ with container_data_editor:
         # st.subheader(f'{tipo_forma_abastecimento} no município de {municipio}')
         with colcenter5:
             edited_df = st.data_editor(dados_municipio[['Nome da Forma de Abastecimento','Código Forma de abastecimento','Situação']].set_index('Código Forma de abastecimento'),
-                                       width=400, hide_index=True, column_config={"Situação": st.column_config.SelectboxColumn("Situação",['Sem informação','Funcionando','Parada/danificada'])
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                           
-                                           
+                                       width=400, hide_index=True, column_config={"category":st.column_config.SelectboxColumn("Situação",['Sem informação','Funcionando','Parada/danificada'])})
+                                                                                     
                                                                                                                                                                                                                                                                                                                 
             # Cria um botão para enviar a atualização e redefine o estado da sessão quando clicado           
             submit = st.button('Enviar atualização!', type='primary')#, on_click=reset)
