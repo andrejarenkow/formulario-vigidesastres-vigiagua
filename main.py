89e5c54f1133f6d20f2d00ce60ac7c3222280baa
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-17 10:44:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ with container_data_editor:
                         situacao_atualizada = st.selectbox(dados_x.iloc[i]['Nome da Forma de Abastecimento'],options=opcoes_situacao,
                                                            index=0,
                                                            key=f'situacao_{i}')
-                        dados_x.at[i, 'Situação'] = situacao_atualizada
+                        dados_x.at[i-len(dados_x)-1, 'Situação'] = situacao_atualizada
                 return dados_x
             dados_atualizados = renderizar_editor(dados_municipio)
             dados_atualizados.dropna(how='any', inplace=True)
@@ -183,9 +183,9 @@ with container_data_editor:
             
             # Verifica se o botão de envio foi clicado
             if submit:
-                for i in range(len(dados_atualizados)):
-                    dados_atualizados.at[i, 'Situação'] = st.session_state[f'situacao_{i}']
-                    st.write(dados_atualizados.at[i, 'Situação'])
+                #for i in range(len(dados_atualizados)):
+                #    dados_atualizados.at[i, 'Situação'] = st.session_state[f'situacao_{i}']
+                #    st.write(dados_atualizados.at[i, 'Situação'])
                 
                 dados_antigos = dados.copy()
                 dados.reset_index(drop=True, inplace=True)
