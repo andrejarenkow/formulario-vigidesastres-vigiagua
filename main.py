66d5f7013239c97272873d82f4b25f4ce89e0b46
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-17 00:18:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -131,12 +131,12 @@ st.markdown(f'<h1 style="text-align: center;color:#FFFFFF;font-size:16px;">{"Mar
 opcoes_situacao = ['Sem informação', 'Funcionando', 'Parada/danificada']      
 container_data_editor = st.container()
 with container_data_editor:
-    col4,colcenter5,col6 = st.columns(3)
+    col4,colcenter5,col6 = st.columns([2,1,2])
     try:
         # Tenta executar as seguintes instruções
         # Comentários abaixo são comentários de código, não estão habilitados no momento devido ao formato da entrada.
         # st.subheader(f'{tipo_forma_abastecimento} no município de {municipio}')
-        with colcenter2:
+        with colcenter5:
             #edited_df = st.data_editor(dados_municipio[['Nome da Forma de Abastecimento','Código Forma de abastecimento','Situação']].set_index('Código Forma de abastecimento'),
              #                          use_container_width=True, hide_index=True, column_config={"category":st.column_config.SelectboxColumn("Situação",default='Sem informação',options=['Sem informação','Funcionando','Parada/danificada'], width='medium'),
               #                                                                                   'category':st.column_config.Column(label='Nome',width='medium')})
