c2f1bb83ad76b493fc8c6b6883ceac6ae4a5bf4a
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-17 00:33:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ with container_data_editor:
                                                            index=opcoes_situacao.index(dados.iloc[i]['Situação']) if dados.iloc[i]['Situação'] in opcoes_situacao else 0,
                                                            key=f'situacao_{i}')
                         st.markdown(f"""
-                            #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(6) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div.st-emotion-cache-0.e1f1d6gn0 > div > div > div > div
+                            #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(6) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div
                             {{    
                                 box-shadow: 0px 0px 5px 5px rgba(255, 255, 255, 0.25);
                                 border: 2px solid white;
