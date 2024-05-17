5e676f66a37783e9662780e37a8cc8084c3ccc7e
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-16 21:03:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,8 @@ with container_data_editor:
             
             st.markdown(f'''
             <style>
-            #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(5) > div > div > div > div.st-emotion-cache-keje6w.e1f1d6gn3 > div > div > div > div:nth-child(3) > div {{
+            #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(5) > div > div > div > div.st-emotion-cache-fplge5.e1f1d6gn3 > div > div > div > div:nth-child(3) > div > button                
+            {{
                 display: flex;
                 justify-content: center; /* Centraliza horizontalmente */
                 align-items: center;
