fb6b1135eb03fd7b39f68444997316d1bec5f0bc
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-21 22:35:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ with container_data_editor:
                 st.success(f'Foram realizadas {len(mudancas)} atualizações!', icon="✅")
                 st.markdown(f"""
                 <style>
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+3))}) 
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+7))}) 
                 {{
                     display: flex;
                     justify-content: center; /* Centraliza horizontalmente */
@@ -255,13 +255,13 @@ with container_data_editor:
                 }}
                 </style>
                 <style>
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+3))}) > div > div > div > div
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+7))}) > div > div > div > div
                 {{text-align:center;}}
                 </style>
                 """, unsafe_allow_html=True)
                 st.markdown(f"""
                 <style>
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+4))}) 
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+8))}) 
                 {{
                     display: flex;
                     justify-content: center; /* Centraliza horizontalmente */
@@ -270,7 +270,7 @@ with container_data_editor:
                 }}
                 </style>
                 <style>
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+4))}) > div > div > div > div
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+8))}) > div > div > div > div
                 {{text-align:center;}}
                 </style>
                 """, unsafe_allow_html=True)
