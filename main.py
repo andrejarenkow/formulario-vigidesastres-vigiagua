64582a528f9dbb576b680ad9bed67f5f08fef443
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-21 22:52:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -261,7 +261,7 @@ with container_data_editor:
                 """, unsafe_allow_html=True)
                 st.markdown(f"""
                 <style>
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(8) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+7))}) > div > div > div
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(9) > div > div > div > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div:nth-child({str(int(quantos_selectbox+7))}) > div > div > div
                 {{
                     display: flex;
                     justify-content: center; /* Centraliza horizontalmente */
