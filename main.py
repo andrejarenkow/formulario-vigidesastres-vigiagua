b0730a142da761c628fa16e0b36f7c30704c7188
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-03 12:06:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd  # Importa a biblioteca pandas e a renomeia como pd
 import streamlit as st # Importa a biblioteca streamlit e a renomeia como st
 from streamlit_gsheets import GSheetsConnection
+import numpy as np
 
 pd.options.display.max_columns=None
 st.set_page_config(
